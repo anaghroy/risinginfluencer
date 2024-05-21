49bde5281514a1035cd590dd3df18f07cce47d10
ANAGH ROY <161459471+anaghroy@users.noreply.github.com>
2024-05-21 14:10:03 +0530

    Add files via upload

---
+++ js/service.js
@@ -0,0 +1,19 @@
+//service starts here
+
+//scroll Reveal starts here
+ScrollReveal({ 
+  reset: true,
+  distance:'60px',
+  duration:2500,
+  delay:400
+  });
+  ScrollReveal().reveal('.heading_container', { delay: 500, origin:'right' });
+  ScrollReveal().reveal('.pricing-table', { delay: 500, origin:'buttom' });
+  ScrollReveal().reveal('.pptable-bodytable-price', { delay: 500, origin:'buttom' });
+  ScrollReveal().reveal('.service_container', { delay: 500, origin:'right' });
+  ScrollReveal().reveal('.detail-box', { delay: 600, origin:'left' });
+  ScrollReveal().reveal('.img-box', { delay: 500, origin:'right' });
+  
+//service ends here
+
+  
